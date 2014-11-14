a1b07759b6ad6f3c67e31bfd5f642f01bf4264b2
David Basilio Espitalher <dbasilioesp@gmail.com>
2014-11-14 00:25:16 -0200

    Created Two Lights and LightMouseRotate

--- ComputerGraphic/TwoLights/Main.cpp
+++ ComputerGraphic/TwoLights/Main.cpp
@@ -11,15 +11,39 @@ void init(void)
 	glShadeModel (GL_SMOOTH);
 
 	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
-	GLfloat light_color[] = { 0.0, 1.0, 0.0, 0.0 };
-	
+	GLfloat light_color[] = { 0.2, 0.8, 0.2, 0.0 };
+
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_color);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light_color);
-	
+
+	//GLfloat spot_direction[] = { -1.0, -1.0, 0.0 };
+	//glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 45.0);
+	//glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
+	//glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 10);
+
+	GLfloat light1_ambient[] = { 0.2, 0.2, 0.2, 1.0 };
+	GLfloat light1_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light1_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light1_position[] = { -2.0, 2.0, 1.0, 1.0 };
+	GLfloat spot_direction[] = { -1.0, -1.0, 0.0 };
+
+	glLightfv(GL_LIGHT1, GL_AMBIENT, light1_ambient);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, light1_specular);
+	glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+	glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, 1.0);
+	glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.0);
+	glLightf(GL_LIGHT1, GL_QUADRATIC_ATTENUATION, 0.0);
+
+	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, -45.0);
+	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spot_direction);
+	glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 2.0);
+
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
 	glEnable(GL_DEPTH_TEST);
 }
 
@@ -27,13 +51,13 @@ void init(void)
 void display(void)
 {
 	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+
 	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
 	GLfloat mat_shininess[] = { 50.0 };
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 	glutSolidSphere (1.0, 20, 16);
-	
+
 	glFlush ();
 }
 
