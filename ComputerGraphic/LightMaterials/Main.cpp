547a69d92b6d1b8dc291d24efd358497b7a451ed
David Basilio Espitalher <dbasilioesp@gmail.com>
2014-11-22 10:07:57 -0200

    Update Main.cpp

--- ComputerGraphic/LightMaterials/Main.cpp
+++ ComputerGraphic/LightMaterials/Main.cpp
@@ -28,75 +28,70 @@ void init(void)
 
 void display(void)
 {
-	GLfloat position[] = {0.0, 0.0, 1.5, 1.0 };
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	gluLookAt(0, 0, -10, 0, 0, 0, 0, 1, 0);
+
+	GLfloat position[] = {0.0, 0.0, -3.0, 1.0 };
+	GLfloat light_color[] = { 0.0, 1.0, 0.0, 0.0 };
+	GLfloat model_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
+
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_color);
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, model_ambient);
+
 	/*  draw sphere in first row, first column
 	*  diffuse reflection only; no ambient or specular  
 	*/
-   glPushMatrix();
-   glTranslatef (-3.75, 3.0, 0.0);
-   glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
-   glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-   glMaterialfv(GL_FRONT, GL_SPECULAR, no_mat);
-   glMaterialfv(GL_FRONT, GL_SHININESS, no_shininess);
-   glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
-   glutSolidSphere(1.0, 16, 16);
-   glPopMatrix();
+	glPushMatrix();
+	glTranslatef (-3.75, 0.0, 0.0);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, no_mat);
+	glMaterialfv(GL_FRONT, GL_SHININESS, no_shininess);
+	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
+	glutSolidSphere(1.0, 16, 16);
+	glPopMatrix();
 
-   /*  draw sphere in first row, second column
-   *  diffuse and specular reflection; low shininess; no ambient
-   */
-   glPushMatrix();
-   glTranslatef (-1.25, 3.0, 0.0);
-   glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
-   glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-   glMaterialfv(GL_FRONT, GL_SHININESS, low_shininess);
-   glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
-   glutSolidSphere(1.0, 16, 16);
-   glPopMatrix();
+	/*  draw sphere in first row, second column
+	*  diffuse and specular reflection; low shininess; no ambient
+	*/
+	glPushMatrix();
+	glTranslatef (-1.25, 0.0, 0.0);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, low_shininess);
+	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
+	glutSolidSphere(1.0, 16, 16);
+	glPopMatrix();
 
-   /*  draw sphere in first row, third column
-   *  diffuse and specular reflection; high shininess; no ambient
-   */
-   glPushMatrix();
-   glTranslatef (1.25, 3.0, 0.0);
-   glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
-   glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-   glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-   glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
-   glutSolidSphere(1.0, 16, 16);
-   glPopMatrix();
+	/*  draw sphere in first row, third column
+	*  diffuse and specular reflection; high shininess; no ambient
+	*/
+	glPushMatrix();
+	glTranslatef (1.25, 0.0, 0.0);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+	glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
+	glutSolidSphere(1.0, 16, 16);
+	glPopMatrix();
 
-   /*  draw sphere in first row, fourth column
-   *  diffuse reflection; emission; no ambient or specular refl.
-   */
-   glPushMatrix();
-   glTranslatef (3.75, 3.0, 0.0);
-   glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
-   glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-   glMaterialfv(GL_FRONT, GL_SPECULAR, no_mat);
-   glMaterialfv(GL_FRONT, GL_SHININESS, no_shininess);
-   glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
-   glutSolidSphere(1.0, 16, 16);
-   glPopMatrix();
-
-   glPushMatrix();
-   glTranslatef(0.0, 0.0, -5.0);
-
-   glPushMatrix();
-   //glRotated((GLdouble) spin, 1.0, 0.0, 0.0);
-   glLightfv(GL_LIGHT0, GL_POSITION, position);
-   glDisable(GL_LIGHTING);
-
-   glTranslated(0.0, 0.0, 1.5);
-   glColor3f(0.0, 1.0, 1.0);
-   glutWireCube(0.1);
-
-   glEnable(GL_LIGHTING);
-   glPopMatrix();
+	/*  draw sphere in first row, fourth column
+	*  diffuse reflection; emission; no ambient or specular refl.
+	*/
+	glPushMatrix();
+	glTranslatef (3.75, 0.0, 0.0);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, no_mat);
+	glMaterialfv(GL_FRONT, GL_SHININESS, no_shininess);
+	glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
+	glutSolidSphere(1.0, 16, 16);
+	glPopMatrix();
 
 	glFlush();
 }
@@ -117,8 +112,8 @@ void mouse(int button, int state, int x, int y)
 	{
 	case GLUT_LEFT_BUTTON:
 		if(state == GLUT_DOWN){
-			spin = (spin + 30) % 360;
-			glutPostRedisplay();
+			
+			//glutPostRedisplay();
 		}
 		break;
 	default:
@@ -130,7 +125,7 @@ int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(500, 500);
+	glutInitWindowSize(800, 600);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow(argv[0]);
 
